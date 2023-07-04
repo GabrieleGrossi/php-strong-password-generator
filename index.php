2de7bbb8ae99a0ee337e2efdc26971104885d8bc
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-04 16:23:15 +0200

    Add some bootstrap and start create function

--- index.php
+++ index.php
@@ -6,16 +6,12 @@ Milestone 2
 -->
 
 <?php
-    $length = $_GET['passwordLength'];
-    
-    function generatePsw() {
-        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
-        for ($i = 1; $i < $length; $i++) {
+    $pswLength = $_GET['passwordLength'];
+    $charactersList = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!£$%&/()^[]{}@#§";
 
-            $n = rand(0, count($alphabet)-1);
-            $pass[$i] = $alphabet[$n];
-        }
-        return $pass;
+    function generatePsw($length, $characters) {
+        $password= [];
+        $availableChar= strlen($characters)
     }
 ?>
 
@@ -28,18 +24,29 @@ Milestone 2
     <title>Password generator PHP</title>
 </head>
 <body>
-    <h1>
-        Strong Password Generator
-    </h1>
-    <h2>
-        Generate a secure password 
-    </h2>
+    <div class="row">
+        <div class="col-12">
+            <h1>
+                Strong Password Generator
+            </h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <h2>
+                Generate a secure password 
+            </h2>
+        </div>
+    </div>
     <form action="./index.php" method="GET">
-        <label for="passwordLength">Password length:</label>
-        <input type="text" name="passwordLength" id="passwordLength">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-between">
+                <label for="passwordLength">Password length:</label>
+                <input type="text" name="passwordLength" id="passwordLength">
+            </div>
+        </div>
         <button type="submit">Submit</button>
     </form>
-    <?php echo $pass
-    ?>
+    
 </body>
 </html>
