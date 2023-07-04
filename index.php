78019402523771856e3579d9b18a3e60178cf112
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-04 15:36:58 +0200

    Add exercise

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!-- 
+Milestone 1
+    Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. Scriviamo tutto (logica e layout) in un unico file index.php
+Milestone 2
+    Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+-->
+
+<?php
+    
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +19,16 @@
     <title>Password generator PHP</title>
 </head>
 <body>
-    
+    <h1>
+        Strong Password Generator
+    </h1>
+    <h2>
+        Generate a secure password 
+    </h2>
+    <form action="./index.php" method="GET">
+        <label for="passwordLength">Password length:</label>
+        <input type="text" name="passwordLength" id="passwordLength">
+        <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
