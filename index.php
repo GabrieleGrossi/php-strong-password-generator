b17eaa0eac2df88f54f5d2f2a7459669f790bcea
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-04 17:27:23 +0200

    exercise done

--- index.php
+++ index.php
@@ -15,36 +15,39 @@ require __DIR__ . "./function.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
     <title>Password generator PHP</title>
 </head>
 <body>
-    <div class="row">
+    <div class="row text-center">
         <div class="col-12">
             <h1>
                 Strong Password Generator
             </h1>
         </div>
     </div>
-    <div class="row">
+    <div class="row text-center">
         <div class="col-12">
             <h2>
                 Generate a secure password 
             </h2>
         </div>
     </div>
-    <form action="./index.php" method="GET">
-        <div class="row">
-            <div class="col-12 d-flex justify-content-between">
-                <label for="passwordLength">Password length:</label>
-                <input type="number" name="pswLength" id="pswLength">
+    <div class="container">
+        <form action="./index.php" method="GET">
+            <div class="row">
+                <div class="col-12 mb-5 text-center">
+                    <label for="passwordLength">Password length:</label>
+                    <input type="number" name="pswLength" id="pswLength">
+                    <button type="submit" class="btn btn-outline-dark">Generate</button>
+                </div>
             </div>
-        </div>
-        <button type="submit">Generate</button>
-    </form>
-    <p class="text-center pb-5 pt-5">
-        <?php 
-            echo "La password generata è: " . $generatedPassword;
-        ?>
-    </p>
+        </form>
+        <p class="text-center">
+            <?php 
+                echo "La password generata è: " . $generatedPassword;
+            ?>
+        </p>
+    </div>
 </body>
 </html>
