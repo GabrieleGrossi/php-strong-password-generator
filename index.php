b5854edb9d3dd6b8cc8ff8e14a4b334d2c4cacad
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-04 16:10:46 +0200

    Add bootstrap

--- index.php
+++ index.php
@@ -8,7 +8,15 @@ Milestone 2
 <?php
     $length = $_GET['passwordLength'];
     
-    function
+    function generatePsw() {
+        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+        for ($i = 1; $i < $length; $i++) {
+
+            $n = rand(0, count($alphabet)-1);
+            $pass[$i] = $alphabet[$n];
+        }
+        return $pass;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +24,7 @@ Milestone 2
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <title>Password generator PHP</title>
 </head>
 <body>
@@ -30,5 +39,7 @@ Milestone 2
         <input type="text" name="passwordLength" id="passwordLength">
         <button type="submit">Submit</button>
     </form>
+    <?php echo $pass
+    ?>
 </body>
 </html>
